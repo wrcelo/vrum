b57a28281975354383cfb1c54d1b6088a1727d71
wrcelo <marcelomramalho@gmail.com>
2025-04-12 18:56:45 -0300

    classe de resposta

---
+++ src/Wrcelo.MottuApp.Core/Response/ResponseModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wrcelo.VrumApp.Core.Response
+{
+    public class ResponseModel<T>
+    {
+        public ResponseModel(T data, bool success, string message)
+        {
+            Data = data;
+            Success = success;
+            Message = message;
+        }
+
+        public T Data { get; set; }
+        public bool Success { get; set; }
+        public string Message { get; set; }
+    }
+    
+}
